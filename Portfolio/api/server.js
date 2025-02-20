20af16a4db615c16908f5751c2b369a9cbb92ae1
Shahrukh Showkath <shahrukhshowkath@gmail.com>
2025-02-20 11:11:01 -0600

    minor change to server

--- Portfolio/api/server.js
+++ Portfolio/api/server.js
@@ -5,7 +5,8 @@ import { JSDOM } from 'jsdom';
 import cors from 'cors';
 
 const app = express();
-const port = process.env.Port || 3001; 
+const port = process.env.PORT || 3001; 
+
 
 app.use(cors());
 
